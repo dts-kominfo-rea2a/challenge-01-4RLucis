a50205313ebe0033a84c38ba8c0d84b1c9f3fd26
Samuel Budy Dharwan <dharwan.helena@gmail.com>
2022-06-14 11:30:55 +0700

    Change index.js

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+// User's Color
+const firstUserColor = new Set(["Yellow", "Pink", "White", "Purple"]);
+const secondUserColor = new Set(["Blue", "Black", "Grey"]);
+
+// User's Education
+const firstUserEdu = [
+  { name: "SD 01", city: "Jakarta", graduate: "2016" },
+  { name: "SMP 02", city: "Jakarta", graduate: "2019" },
+  { name: "SMA 03", city: "Tangerang" },
+];
+
+const secondUserEdu = [
+  { name: "SD 02", city: "Jakarta", graduate: "2010" },
+  { name: "SMP 03", city: "Bogor", graduate: "2013" },
+  { name: "SMA 01", city: "Surabaya", graduate: "2016" },
+  { name: "Universitas Maju", city: "Tangerang" },
+];
+
+// User's Restaurant
+const firstUserResto = new Set([
+  "Bento",
+  "Suhi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Bento",
+  "Eggy",
+  "Padang",
+  "Tteok",
+  "Sushi",
+  "Sushi",
+]);
+
+const secondUserResto = new Set([
+  "Tempura",
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Padang",
+  "Katsu",
+  "Geprek",
+  "Pancake",
+  "Eggy",
+]);
+
+// User's property
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: firstUserColor,
+  isHavePet: "No",
+  education: firstUserEdu,
+  favoriteRestaurant: firstUserResto,
+};
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColor: firstUserColor,
+  isHavePet: "No",
+  education: secondUserEdu,
+  favoriteRestaurant: secondUserResto,
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
